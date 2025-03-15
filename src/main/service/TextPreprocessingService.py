6a39b777b3a090956e4da0445ca4a8134cb0ff93
kaviyanSL <kaviyan.lari.71@gmail.com>
2025-03-15 19:48:42 +0330

    prompt spell checker

--- src/main/service/TextPreprocessingService.py
+++ src/main/service/TextPreprocessingService.py
@@ -5,3 +5,8 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %
 class TextPreprocessingService:
     def __init__():
         pass
+
+    def prompt_spell_correction(self,prompt):
+        corrected_prompt = TextBlob(prompt)
+        logging.info(f"prompt spell is corrected:{corrected_prompt}")
+        return corrected_prompt
