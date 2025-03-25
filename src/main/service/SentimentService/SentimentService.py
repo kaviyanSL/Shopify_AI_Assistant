d23c13fc7510509d654700f9ba2aeb9f27d889b5
kaviyanSL <kaviyan.lari.71@gmail.com>
2025-03-25 17:26:44 +0330

    defining the first approach for sentiment analysis for comments

---
+++ src/main/service/SentimentService/SentimentService.py
@@ -0,0 +1,17 @@
+from transformers import pipeline
+class SentimentService:
+    def __init__(self):
+        self.model_five_classes = "nlptown/bert-base-multilingual-uncased-sentiment"
+
+    def model_pipline(self):
+        model = pipeline("sentiment-analysis", self.model_five_classes)
+        return model
+    
+    def sentiment_analaysis (self,list_of_comments):
+        model = self.model_pipline()
+        sentiment_results = model(list_of_comments)
+        list_results_and_reviews = []
+        for reviews,results in zip (list_of_comments,sentiment_results):
+            list_results_and_reviews.append(zip(reviews,results))
+        return list_results_and_reviews
+
