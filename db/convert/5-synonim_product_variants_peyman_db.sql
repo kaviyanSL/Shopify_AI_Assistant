4b529e53e7d8945b640b65750240429443767628
kaviyanSL <kaviyan.lari.71@gmail.com>
2025-03-27 13:06:56 +0330

    creating synonim from peyman db

---
+++ db/convert/5-synonim_product_variants_peyman_db.sql
@@ -0,0 +1,5 @@
+CREATE VIEW shopify_search_assistant.products_synonym AS 
+SELECT * FROM shopify_product_peyman.products;
+
+CREATE VIEW shopify_search_assistant.variants_synonym AS 
+SELECT * FROM shopify_product_peyman.product_variants;
